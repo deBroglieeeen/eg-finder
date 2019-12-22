862a1d56172432d3281dadb60d22f744119a8158
deBroglieeeen <u825246d@ecs.osaka-u.ac.jp>
2019-12-23 06:13:50 +0900

    improve

--- index.js
+++ index.js
@@ -17,46 +17,46 @@ const bot = new line.Client(line_config);
 // -----------------------------------------------------------------------------
 let example_sentences = ""
 // -----------------------------------------------------------------------------
-// import { stringify } from "querystring";
-// import { request } from "http";
+var qs = require("querystring");
+var http = require("http");
 
-// var options = {
-//   "method": "POST",
-//   "hostname": [
-//     "sealang",
-//     "net"
-//   ],
-//   "path": [
-//     "pm",
-//     "bitext.pl"
-//   ],
-//   "headers": {
-//     "Content-Type": "application/x-www-form-urlencoded",
-//     "User-Agent": "PostmanRuntime/7.20.1",
-//     "Accept": "*/*",
-//     "Cache-Control": "no-cache",
-//     "Postman-Token": "4700a0e6-91ef-4e95-bc4a-7d39c275166b,0d57394c-ea9a-47c8-bb2c-acf39ce12303",
-//     "Host": "sealang.net",
-//     "Accept-Encoding": "gzip, deflate",
-//     "Content-Length": "129",
-//     "Connection": "keep-alive",
-//     "cache-control": "no-cache"
-//   }
-// };
+var options = {
+  "method": "POST",
+  "hostname": [
+    "sealang",
+    "net"
+  ],
+  "path": [
+    "pm",
+    "bitext.pl"
+  ],
+  "headers": {
+    "Content-Type": "application/x-www-form-urlencoded",
+    //"User-Agent": "PostmanRuntime/7.20.1",
+    "Accept": "*/*",
+    "Cache-Control": "no-cache",
+    //"Postman-Token": "4700a0e6-91ef-4e95-bc4a-7d39c275166b,0d57394c-ea9a-47c8-bb2c-acf39ce12303",
+    "Host": "sealang.net",
+    "Accept-Encoding": "gzip, deflate",
+    "Content-Length": "129",
+    "Connection": "keep-alive",
+    "cache-control": "no-cache"
+  }
+};
 
-// var ce_request = request(options, function (res) {
-//   var chunks = [];
+var ce_request = http.request(options, function (res) {
+  var chunks = [];
 
-//   res.on("data", function (chunk) {
-//     chunks.push(chunk);
-//   });
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
 
-//   res.on("end", function () {
-//     var body = Buffer.concat(chunks);
-//     console.log(body.toString());
-//     example_sentences = body.toString()
-//   });
-// });
+  res.on("end", function () {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+    example_sentences = body.toString()
+  });
+});
 
 // -----------------------------------------------------------------------------
 // ルーター設定
@@ -75,24 +75,24 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
                     text: "ビルマ語はミャンマーの旧名称でその地域で話されていることばです話者は5000万人ほどいます。"
                 }));
             }
-            // else{
-            //     ce_request.write(stringify({ approx: 'W',
-            //       bitectMatch: 'or',
-            //       near: '10',
-            //       return: 'html',
-            //       seaLanguage: 'burmese',
-            //       seaTarget: '',
-            //       type: 'bitext',
-            //       westernLanguage: 'english',
-            //       westernTarget: event.message.text }));
-            //     ce_request.end().then(()=>{
-            //       console.log(example_sentences)
-            //       events_processed.push(bot.replyMessage(event.replyToken,{
-            //         type: "text",
-            //         text: example_sentences
-            //       }));
-            //     });
-            // }
+            else{
+                ce_request.write(qs.stringify({ approx: 'W',
+                  bitectMatch: 'or',
+                  near: '10',
+                  return: 'html',
+                  seaLanguage: 'burmese',
+                  seaTarget: '',
+                  type: 'bitext',
+                  westernLanguage: 'english',
+                  westernTarget: event.message.text }));
+                ce_request.end().then(()=>{
+                  console.log(example_sentences)
+                  events_processed.push(bot.replyMessage(event.replyToken,{
+                    type: "text",
+                    text: example_sentences
+                  }));
+                });
+            }
         }
     });
     // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
