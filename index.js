98a1a997ff94f46e6fb21c9849a3336284754c3e
deBroglieeeen <u825246d@ecs.osaka-u.ac.jp>
2019-12-23 04:52:49 +0900

    changed text

--- index.js
+++ index.js
@@ -14,10 +14,32 @@ const line_config = {
 // Webサーバー設定
 server.listen(process.env.PORT || 3000);
 
-
+const bot = new line.Client(line_config);
 // -----------------------------------------------------------------------------
 // ルーター設定
 server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     res.sendStatus(200);
     console.log(req.body);
+
+    let events_processed = [];
+    req.body.events.forEach((event) => {
+        // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+        if (event.type == "message" && event.message.type == "text"){
+            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
+            if (event.message.text == "ビルマ語？"){
+                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+                events_processed.push(bot.replyMessage(event.replyToken, {
+                    type: "text",
+                    text: "ビルマはミャンマーの旧名称でその地域で話されていることばです話者は5000万人ほどいます。"
+                }));
+            }
+        }
+    });
+    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
+    Promise.all(events_processed).then(
+        (response) => {
+            console.log(`${response.length} event(s) processed.`);
+        }
+    );
 });
+
