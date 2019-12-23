c1160c11f743f1ca589293327ce08ba2dc9e156e
deBroglieeeen <u825246d@ecs.osaka-u.ac.jp>
2019-12-23 22:48:23 +0900

    improve

--- index.js
+++ index.js
@@ -201,7 +201,7 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
                         text: $("cell").children().first().text().trim()
                       }));
 
-                      for(index = 1;index < 7; index++){
+                      for(index = 1;index < 20; index++){
                         events_processed.push(bot.replyMessage(event.replyToken, {
                             type: "text",
                             text: $("cell").children().eq(index).text().trim()
